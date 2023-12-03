b595823e04521f3d1e5ce70f8c20a35e82a83a0b
htringuyen <tringuyenh75@gmail.com>
2023-12-03 17:56:08 +0700

    add cors headers

--- public/index.php
+++ public/index.php
@@ -4,6 +4,17 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 $separator = DIRECTORY_SEPARATOR;
 
+$CORS_ORIGIN_ALLOWED = "http://localhost:3000";
+
+function applyCorsHeaders($origin) {
+    header("Access-Control-Allow-Origin: $origin");
+    header("Access-Control-Allow-Credentials: true");
+    header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Accept, Authorization');
+}
+
+applyCorsHeaders($CORS_ORIGIN_ALLOWED);
+
 $app = \Slimmvc\App::getInstance();
 $app->bind('paths.base', fn() => __DIR__ . $separator.'..');
 //$app->bind('url.path.prefix', fn() => "empalic-backend");
