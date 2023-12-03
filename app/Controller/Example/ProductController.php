65f2d02bc0163a44cc3a8513703bac170c89aaac
htringuyen <tringuyenh75@gmail.com>
2023-12-03 22:31:09 +0700

    clear some conflicts

--- app/Controller/Example/ProductController.php
+++ app/Controller/Example/ProductController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Example;
 
 use App\Model\Product;
-<<<<<<< HEAD
 use function App\Controller\view;
 
 class ProductController {
@@ -31,31 +30,5 @@ class ProductController {
     private function view(string $string, array $compact)
     {
     }
-
 }
-=======
-use Slimmvc\Http\HttpRequest;
-use Slimmvc\Http\HttpResponse;
 
-// $router->addRoute("GET", "/dev/product",
-// handler: [\App\Controller\Example\ProductController::class, "getAllProducts"],
-// );
-
-class ProductController
-{
-    // get all products
-    public function getAllProducts(HttpResponse $response, HttpRequest $request)
-    {
-        $products = Product::all();
-
-        $data = array_map(function (Product $product) {
-           return $product->toSerializationArray();
-        }, $products);
-
-        $response->setType(HttpResponse::JSON);
-        $response->setContent($data);
-
-        return $response;
-    }
-}
->>>>>>> d1f315e7ae224bd1f1c5df923667b792d0b84367
