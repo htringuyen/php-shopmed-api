836bfa3c445aa46c708e8f37a470038eb6116bd9
quangtri <quangtri3032001@gmail.com>
2023-12-06 05:15:21 +0700

    add product + cart controller

---
+++ app/Controller/Business/Cart/CartController.php
@@ -0,0 +1,123 @@
+<?php
+
+namespace App\Controller\Business\Cart;
+
+use App\Model\CartItem;
+use App\Model\Product;
+use Slimmvc\Http\HttpRequest;
+use Slimmvc\Http\HttpResponse;
+use Slimmvc\Http\TokenAuthentication;
+use Exception;
+
+class CartController
+{
+    public function getAllCartItems(HttpRequest $request, HttpResponse $response, TokenAuthentication $auth){
+        $userId = $auth->getUserId();
+        $cartItems = CartItem::query()->where("userId", $userId, "=")->all();
+        $data = [];
+        foreach($cartItems as $cartItem){
+            $element = [
+                "id" => $cartItem->id,
+                "price" => $cartItem->product->price,
+                "productName" => $cartItem->product->name,
+                "productId" => $cartItem->product->id,
+                "quantity" => $cartItem->quantity
+            ];
+            array_push($data , $element);
+        }
+
+        $response->setType(HttpResponse::JSON);
+        $response->setContent($data);
+        return $response;
+
+    }
+
+
+    public function addNewCartItem(HttpRequest $request, HttpResponse $response, TokenAuthentication $auth){
+        $cartItem = new CartItem();
+        $userId = $auth->getUserId();
+
+        $cartItem->userId = $userId;
+        $cartItem->productId = $request->requestParam("productId");
+        $cartItem->quantity = $request->requestParam("quantity") ?? 1;
+        $cartItem->createdAt = date("Y-m-d H:i:s");
+        $cartItem->isActive = true;
+
+        try {
+            $cartItem->save();
+            $response->setType(HttpResponse::JSON);
+            $response->setContent($cartItem->toSerializationArray());
+            return $response;
+        } catch (Exception $e) {
+            $response->setType(HttpResponse::JSON);
+            $response->setStatus(400);
+            $response->setContent(["message" => $e->getMessage()]);
+            return $response;
+        }
+    }
+
+    public function increaseQuantity(HttpRequest $request, HttpResponse $response, TokenAuthentication $auth)
+    {
+        $id = $request->requestParam("cartItemId");
+
+        $cartItem = CartItem::where("id", $id)->first();
+
+        if (!$cartItem) {
+            $response->setType(HttpResponse::JSON);
+            $response->setContent(["message" => "Cart item not found"]);
+            $response->setStatus(404);
+            return $response;
+        }
+
+        $cartItem->quantity = intval($cartItem->quantity) + 1;
+        $cartItem->save();
+
+        $response->setType(HttpResponse::JSON);
+        $response->setContent($cartItem->toSerializationArray());
+        return $response;
+    }
+
+    public function decreaseQuantity(HttpRequest $request, HttpResponse $response, TokenAuthentication $auth){
+        $id = $request->requestParam("cartItemId");
+
+        $cartItem = CartItem::where("id", $id)->first();
+
+        if (!$cartItem) {
+            $response->setType(HttpResponse::JSON);
+            $response->setContent(["message" => "Cart item not found"]);
+            $response->setStatus(404);
+            return $response;
+        }
+        if ($cartItem->quantity == 1) {
+            $response->setType(HttpResponse::JSON);
+            $response->setContent(["message" => "min Quantity reached"]);
+            return $response;
+        }
+        $cartItem->quantity = intval($cartItem->quantity) - 1;
+        $cartItem->save();
+
+        $response->setType(HttpResponse::JSON);
+        $response->setContent($cartItem->toSerializationArray());
+        return $response;
+    }
+
+    public function deleteFromCart(HttpRequest $request, HttpResponse $response, TokenAuthentication $auth)
+    {
+        $id = $request->requestParam("cartItemId");
+
+        $cartItem = CartItem::where("id", $id)->first();
+
+        if (!$cartItem) {
+            $response->setType(HttpResponse::JSON);
+            $response->setContent(["message" => "Cart item not found"]);
+            $response->setStatus(404);
+            return $response;
+        }
+
+        //delete
+        $cartItem->delete();
+        $response->setType(HttpResponse::JSON);
+        $response->setContent($cartItem->toSerializationArray());
+        return $response;
+    }
+}
--- app/routes_business.php
+++ app/routes_business.php
@@ -2,6 +2,8 @@
 
 use App\Controller\Util\ReadImageController;
 use Slimmvc\Routing\Router;
+use App\Controller\Business\Product\ProductController;
+use App\Controller\Business\Cart\CartController;
 
 /** Author: ..... */
 
@@ -9,7 +11,18 @@ use Slimmvc\Routing\Router;
 
 return function (Router $router) {
     $BASE_PATH = "/api/v1/business";
+    //product
+    $router->addRoute('GET', $BASE_PATH."/products", [ProductController::class, "getAll"]);
+    $router->addRoute('GET', $BASE_PATH."/products/{id}", [ProductController::class, "getProductDetail"]);
+
+    //cart 
+    $router->addRoute('GET', $BASE_PATH."/cart", [CartController::class, "getAllCartItems"] , protected: true  );
+    $router->addRoute('POST', $BASE_PATH."/cart", [CartController::class, "addNewCartItem"] , protected: true, requiredParams: ["productId"] );
+    $router->addRoute('DELETE', $BASE_PATH."/cart", [CartController::class, "deleteFromCart"] , protected: true, requiredParams: ["cartItemId"]);
 
 
+    //cart item
+    $router->addRoute('POST', $BASE_PATH."/cartitem/increase_quantity", [CartController::class, "increaseQuantity"] , protected: true, requiredParams: ["cartItemId"]);
+    $router->addRoute('POST', $BASE_PATH."/cartitem/decrease_quantity", [CartController::class, "decreaseQuantity"] , protected: true, requiredParams: ["cartItemId"]);
 
 };
